513e010ecfed30f37e5c2fbe2b1ad3c36da27dad
Marlou <marloualbion@gmail.com>
2025-11-11 18:36:21 +0800

    Week 1: First Python Script - 6:34 PM

---
+++ week1_hello.py
@@ -0,0 +1,9 @@
+# week1_hello.py - Python Restart: Nov 11, 2025 6:23 PM
+name = input("Enter your name: ")
+role = input("Your current role: ")
+
+print(f"\nHellow {name}!")
+print(f"You're a {role} -> becoming a Python Automation Pro")
+print("VS Code + Github = LIVE in 2 hours")
+print("Time started: 6:23 PM PH")
+
