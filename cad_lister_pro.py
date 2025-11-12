11619ffa3b196555a690444ef0c927a10bbc54be
Marlou <marloualbion@gmail.com>
2025-11-12 20:53:53 +0800

    Day 2: cad_lister_pro.py updates - 10:30 PM PH

--- cad_lister_pro.py
+++ cad_lister_pro.py
@@ -1,7 +1,7 @@
 # cad_lister_pro.py - P 2,000 (idk if true) Client tool
 import os
-import pandas as pd         # what is 'pd'
-import webbrowser           # what is this for?
+import pandas as pd         # pd = nickname for pandas but I can name anything
+import webbrowser           # opens files in your default app
 
 print("CAD TO EXCEL AUTOMATOR - v2 \n")
 
@@ -13,14 +13,29 @@ if not os.path.exists(folder):
 
 else:
     dwg_files = [
-        y for y in os.listdir(folder)       # I change to 'y' because it 'this_file' is not defined
+        y for y in os.listdir(folder)       # [ OUTPUT for LOOP_VAR in list if condition ]
         if y.lower().endswith(".dwg")
     ]
 
     # export to excel
-    df = pd.DataFrame(dwg_files, columns=["CAD-FILE"])      # what is this?
-    # output_file = input("Enter the name of the file: ").endswith(".xlsx")  - I tried this one but didnt work.
-    output_file = "CAD_Report.xlsx"             # What happens if the file aready exist?
+    df = pd.DataFrame(dwg_files, columns=["CAD-FILE"])      # df = "DataFrame" = Excel table in Python
+    # output_file = input("Enter the name of the file: ").endswith(".xlsx")  - .endswith() returns boolean
+
+#   If i want user input
+#   name = input("Enter file name: ")
+#   if not name.endswith(".xlsx"):
+#       name += ".xlsx"
+#   output_file = name
+
+    output_file = "CAD_Report.xlsx"             # if the file aready exist, It overwrites silently
+
+    # Adding timeframe is great
+    # import datetime
+    # timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")    
+    # output_file = f"CAD_Report_{timestamp}.xlsx"
+    # result - > CAD_Report_20251112_2215.xlsx
+
+
     df.to_excel(output_file, index=False)             
 
     print(f"Success! {len(dwg_files)} files -> {output_file}")
